ab5df4c1814f7ff8fa270b4858cdf98fb66376f3
Enrique Villarreal <evillarreal@elpuig.xeill.net>
2017-03-25 16:57:41 +0100

    Corrige nivel de acceso y añade valor generado en Armari

--- src/main/java/com/coopux/Entities/Armari.java
+++ src/main/java/com/coopux/Entities/Armari.java
@@ -12,10 +12,11 @@ import java.util.Set;
 public class Armari {
     @Id
     @Column
-    int id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
     @Column(length = 60)
-    String nom;
+    private String nom;
 
     @OneToMany(targetEntity = Prestatge.class)
     @JoinColumn(name = "id")
