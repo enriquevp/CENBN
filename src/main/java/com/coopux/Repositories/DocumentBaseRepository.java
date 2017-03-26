3028af027048d181ffe2160e28f0b6a1cf913b7b
Enrique Villarreal <chooseadarkday@gmail.com>
2017-03-26 10:48:40 +0200

    Reescribe el DocumentBaseRepository
    Las consultas ahora hacen JOIN correctamente en la colección de Idiomas, y automáticamente se refieren al tipo del repositorio que las invoca.

--- src/main/java/com/coopux/Repositories/DocumentBaseRepository.java
+++ src/main/java/com/coopux/Repositories/DocumentBaseRepository.java
@@ -1,17 +1,27 @@
 package com.coopux.Repositories;
 
 import com.coopux.Entities.*;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import javax.print.Doc;
 import java.util.List;
 
 /**
  * Created by enrique on 22/03/17.
  */
 public interface DocumentBaseRepository<T extends Document> extends CrudRepository<T, Long> {
-    List<Document> findByTitol(String titol);
-    List<Document> findByMateria(Materia materia);
-    List<Document> findByEditor(Editor editor);
-    List<Document> findByIdiomes(Idioma idioma);
-    List<Document> findByTipus(Tipus tipus);
+    List<T> findByTitol(String titol);
+
+    @Query("select e from #{#entityName} as e where e.materia.nom = ?1 ")
+    List<T> findByMateria(String materia);
+
+    @Query("select e from #{#entityName} as e where e.editor.nom = ?1")
+    List<T> findByEditor(String editor);
+
+    @Query("select e from #{#entityName} e left join e.idiomes idioma where idioma.nom = ?1")
+    List<T> findByIdioma(String idioma);
+
+    @Query("select e from #{#entityName} e where e.tipus.nom = ?1")
+    List<T> findByTipus(Tipus tipus);
 }
