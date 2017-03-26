62d86e64e71737b6d2e4c1b2c847d53df2265f45
Enrique Villarreal <chooseadarkday@gmail.com>
2017-03-26 10:43:00 +0200

    Modificado el Repositorio de Documents.
    Ahora los repositorios de Llibre y Pub.Periodica extenderán de éste.
    Las consultas deberían funcionar correctamente.

--- src/main/java/com/coopux/Repositories/DocumentBaseRepository.java
+++ src/main/java/com/coopux/Repositories/DocumentBaseRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 /**
  * Created by enrique on 22/03/17.
  */
-public interface DocumentRepository extends CrudRepository<Document, Long> {
+public interface DocumentBaseRepository<T extends Document> extends CrudRepository<T, Long> {
     List<Document> findByTitol(String titol);
     List<Document> findByMateria(Materia materia);
     List<Document> findByEditor(Editor editor);
